839810f849766d2ffba57e22b616442d5d2d4fa1
James Ganyo <elikemejay@gmail.com>
2026-05-28 12:43:08 +0000

    Update productApi.js

--- src/services/productApi.js
+++ src/services/productApi.js
@@ -72,7 +72,7 @@ export const handleDelete = async (productId) => {
 // ✅ Fetch total profit
 export const fetchTotalProfit = async () => {
   try {
-    const response = await fetch("/profit", {
+    const response = await fetch("/api/products", {
       credentials: "include",
     });
     if (!response.ok) throw new Error("Failed to fetch total profit");
